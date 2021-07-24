31d84032850cece0e811e498cdfeaac9a57db8bf
SAHARIPRASAD-2907 <67941813+SAHARIPRASAD-2907@users.noreply.github.com>
2021-07-24 10:54:40 +0530

    functions concepts in java script

---
+++ functions/functions-101.js
@@ -0,0 +1,11 @@
+// Function - input (argument) , code , output (return value)
+let fahrenheitToCelsius = function (fahrenheit) {
+    const celsius = (fahrenheit - 32) * (5 / 9)
+    return celsius
+   }
+   let temp70 = fahrenheitToCelsius(70)
+   console.log(temp70)
+   let temp32 = fahrenheitToCelsius(32)
+   console.log(temp32)
+
+   
