30132cd0c66ec99b947887b13a75601a00de2433
S A HARIPRASAD <myharicode@gmail.com>
2022-02-20 14:11:57 +0530

    hangman app complete

---
+++ final-app-css/hangman/app.js
@@ -0,0 +1,40 @@
+const puzzleEl = document.querySelector("#puzzle");
+const gussesEl = document.querySelector("#guesses");
+let game1;
+
+window.addEventListener("keypress", function (e) {
+  const guess = e.key;
+  game1.makeGuess(guess);
+  render();
+});
+
+const startGame = async () => {
+  const puzzle = await getPuzzle("2");
+  game1 = new Hangman(puzzle, 5);
+  render();
+};
+
+const render = () => {
+  puzzleEl.innerHTML = ``;
+  gussesEl.textContent = game1.statueMessage;
+
+  game1.puzzle.split("").forEach((letter) => {
+    const letterEl = document.createElement("span");
+    letterEl.textContent = letter;
+    puzzleEl.appendChild(letterEl);
+  });
+};
+
+document.querySelector("#reset").addEventListener("click", startGame);
+
+startGame();
+
+// getPuzzle("2")
+//   .then((puzzle) => {
+//     console.log(puzzle);
+//   })
+//   .catch((err) => {
+//     console.log(`Error: ${err}`);
+//   });
+
+// fetch API
---
+++ final-app-css/hangman/requests.js
@@ -0,0 +1,24 @@
+const getPuzzle = async (wordCount) => {
+  const response = await fetch(
+    `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`
+  );
+
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.puzzle;
+  } else {
+    throw new Error("Unable to get puzzle");
+  }
+};
+
+// const getPuzzleSync = () => {
+//   const request = new XMLHttpRequest();
+//   request.open("GET", "http://puzzle.mead.io/puzzle?wordCount=2", false);
+//   request.send();
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data = JSON.parse(e.target.responseText);
+//     return data.puzzle;
+//   } else if (e.target.readyState === 4) {
+//     throw new Error("Things did not go well");
+//   }
+// };
